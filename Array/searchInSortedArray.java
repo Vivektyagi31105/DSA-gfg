02db7e5f01479e3cc0680d7600fbabc42aa71d3a
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-31 22:43:16 +0530

    Update searchInSortedArray.java

--- Array/searchInSortedArray.java
+++ Array/searchInSortedArray.java
@@ -14,8 +14,7 @@
 class Solution{
     static int searchInSorted(int arr[], int N, int K)
     {
-        
-        // Your code here
+    
         int low=0;
         int high =N-1;
         while(low<= high){
@@ -29,6 +28,7 @@ class Solution{
              low= mid+1;
             }
         }
+        
         return -1;
     }
 }
