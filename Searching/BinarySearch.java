b5f5e1ec46c6e4b6eb05aa3f145586945498b2c4
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-31 22:44:31 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -10,10 +10,11 @@
 
 class Solution {
     int binarysearch(int arr[], int n, int k) {
-        // code here
+     
         int low=0,high= n-1;
        while(low<= high){
             int mid = low+(high-low)/2;
+           
             if(arr[mid]==k){
                 return mid;
             }else if(arr[mid]>k){
